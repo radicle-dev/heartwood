50dd817762ae53ba45bc4eac0e308580223d8875
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-10-06 10:20:15 +0100

    meta: add nix tooling
    Adds nix tooling to allow people to develop using nix dependencies.
    
    Uses niv to pin nixpkgs, rust-overlay, and niv itself.
    
    This will give Rust tooling that is up to date with rustc-1.64.0.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

---
+++ .nix/shell.nix
@@ -0,0 +1,21 @@
+{ sources ? import ./sources.nix
+, pkgs ? import sources.nixpkgs {
+    overlays = [ (import sources.rust-overlay) ];
+  }
+}:
+let
+  stable = pkgs.rust-bin.stable.latest.default;
+  rust-overlay = stable.override {
+    extensions = [ "rust-src" "rust-analysis" ];
+  };
+  devault = (pkgs.callPackage ./default.nix {});
+in
+  with pkgs;
+  mkShell {
+    name = "development";
+    buildInputs = devault.buildInputs ++ [
+        gcc
+
+        ripgrep
+    ];
+  }
