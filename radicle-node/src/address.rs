d3523ccf7836aaa1718e3d815f5706077c25ee28
Alexis Sellier <alexis@radicle.xyz>
2022-10-18 12:37:01 +0200

    Remove unused type
    Signed-off-by: Alexis Sellier <alexis@radicle.xyz>

--- radicle-node/src/address.rs
+++ radicle-node/src/address.rs
@@ -3,14 +3,3 @@ mod types;
 
 pub use store::*;
 pub use types::*;
-
-#[derive(Debug)]
-pub struct AddressManager<S> {
-    store: S,
-}
-
-impl<S: Store> AddressManager<S> {
-    pub fn new(store: S) -> Self {
-        Self { store }
-    }
-}
