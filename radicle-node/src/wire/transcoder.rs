015fcb50eab125914457d51f3df41aceed4697f5
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-14 15:39:12 +0100

    Complete doc comments for the Transcode and Handshake

--- radicle-node/src/wire/transcoder.rs
+++ radicle-node/src/wire/transcoder.rs
@@ -1,28 +1,41 @@
 use std::convert::Infallible;
 
 // TODO: Implement Try trait once stabilized
+/// Result of a handskae stage
 pub enum HandshakeResult<H: Handshake, T: Transcode> {
+    /// Handshake is not completed; we process to the next handshake stage.
     Next(H, Vec<u8>),
+    /// Handshake is completed; we now can communicate in encrypted way.
     Complete(T, Vec<u8>),
+    /// Handshake has failed with some error.
     Error(H::Error),
 }
 
+/// State machine implementation of a handshake protocol which can be run by
+/// peers.
 pub trait Handshake: Sized {
+    /// The resulting transcoder which will be constructed upon a successful
+    /// handshake
+    type Transcoder: Transcode;
+
     /// Errors which may happen during the handshake.
     type Error: std::error::Error;
 
-    type Transcoder: Transcode;
-
+    /// Constructs a new handshake state machine.
     fn new() -> Self;
+
+    /// Post a new byte stream received by local peer and progress handshake
+    /// protocol.
     fn next_stage(self, input: &[u8]) -> HandshakeResult<Self, Self::Transcoder>;
 }
 
+/// Dumb handshake structure which runs void protocol.
 #[derive(Debug, Default)]
 pub struct NoHandshake;
 
 impl Handshake for NoHandshake {
-    type Error = Infallible;
     type Transcoder = PlainTranscoder;
+    type Error = Infallible;
 
     fn new() -> Self {
         NoHandshake
@@ -37,9 +50,7 @@ impl Handshake for NoHandshake {
 /// and/or encoding.
 pub trait Transcode {
     /// Decodes data received from the remote peer and update the internal state
-    /// of the transcoder, either returning response which must be sent to the
-    /// remote (see [`DecodedData::Remote`]) or data which should be processed
-    /// by the local peer.
+    /// of the transcoder, if necessary.
     fn decrypt(&mut self, data: &[u8]) -> Vec<u8>;
 
     /// Encodes data before sending them to the remote peer.
