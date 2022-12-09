3ba7a022072d1d09efd5ad7606b110a1d1e6488f
Dr Maxim Orlovsky <dr.orlovsky@gmail.com>
2022-12-09 12:55:14 +0100

    Use new cyphernet ECDH-related traits

--- radicle-crypto/src/cyphernet.rs
+++ radicle-crypto/src/cyphernet.rs
@@ -1,89 +1,43 @@
-use std::ops::Deref;
-
-use cyphernet::crypto::{Ec, EcPrivKey, EcPubKey, EcSig};
+use amplify::{From, Wrapper};
+use cyphernet::crypto::{EcPk, EcSig, EcSk, Ecdh};
 use ed25519_compact::x25519;
 
 use crate::{PublicKey, SecretKey, Signature};
 
-// Derivations required for automatic derivations of other types
-#[derive(Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash, Debug)]
-pub struct Ed25519;
+#[derive(Wrapper, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, From)]
+#[wrapper(Deref)]
+pub struct SharedSecret([u8; 32]);
 
-pub type SharedSecret = [u8; 32];
+impl EcPk for PublicKey {}
 
-impl Ec for Ed25519 {
-    type PubKey = PublicKey;
-    type PrivKey = SecretKey;
-    type EcdhSecret = SharedSecret;
-    type EcdhErr = ed25519_compact::Error;
+impl EcSk for SecretKey {
+    type Pk = PublicKey;
+
+    fn to_pk(&self) -> Self::Pk {
+        self.public_key().into()
+    }
 }
 
-impl EcPubKey<Ed25519> for PublicKey {
-    type Raw = [u8; ed25519_compact::PublicKey::BYTES];
+impl Ecdh for SharedSecret {
+    type Sk = SecretKey;
+    type Err = ed25519_compact::Error;
 
-    fn from_raw(raw: Self::Raw) -> Self {
-        PublicKey::from(raw)
-    }
-
-    fn into_raw(self) -> Self::Raw {
-        *self.0.deref()
-    }
-
-    fn ecdh(self, sk: &SecretKey) -> Result<SharedSecret, ed25519_compact::Error> {
-        let xpk = x25519::PublicKey::from_ed25519(&self.0)?;
+    fn ecdh(sk: &Self::Sk, pk: &<Self::Sk as EcSk>::Pk) -> Result<Self, Self::Err> {
+        let xpk = x25519::PublicKey::from_ed25519(&pk.0)?;
         let xsk = x25519::SecretKey::from_ed25519(&sk.0)?;
         let ss = xpk.dh(&xsk)?;
-        Ok(*ss)
+        Ok(Self(*ss))
     }
 }
 
-impl EcPrivKey<Ed25519> for SecretKey {
-    type Raw = [u8; ed25519_compact::SecretKey::BYTES];
+impl EcSig for Signature {
+    type Sk = SecretKey;
 
-    fn from_raw(raw: Self::Raw) -> Self {
-        SecretKey::from(raw)
-    }
-
-    fn into_raw(self) -> Self::Raw {
-        *self.0.deref()
-    }
-
-    fn to_raw(&self) -> Self::Raw {
-        *self.0.deref()
-    }
-
-    fn as_raw(&self) -> &Self::Raw {
-        self.0.deref()
-    }
-
-    fn to_public_key(&self) -> PublicKey {
-        self.0.public_key().into()
-    }
-
-    fn ecdh(&self, pk: PublicKey) -> Result<SharedSecret, ed25519_compact::Error> {
-        let xpk = x25519::PublicKey::from_ed25519(&pk.0)?;
-        let xsk = x25519::SecretKey::from_ed25519(&self.0)?;
-        let ss = xpk.dh(&xsk)?;
-        Ok(*ss)
-    }
-}
-
-impl EcSig<Ed25519> for Signature {
-    type Raw = [u8; ed25519_compact::Signature::BYTES];
-
-    fn from_raw(raw: Self::Raw) -> Self {
-        Signature::from(raw)
-    }
-
-    fn into_raw(self) -> Self::Raw {
-        *self.0
-    }
-
-    fn sign(self, sk: SecretKey, msg: impl AsRef<[u8]>) -> Self {
+    fn sign(self, sk: &SecretKey, msg: impl AsRef<[u8]>) -> Self {
         sk.0.sign(msg, None).into()
     }
 
-    fn verify(self, pk: PublicKey, msg: impl AsRef<[u8]>) -> bool {
+    fn verify(self, pk: &PublicKey, msg: impl AsRef<[u8]>) -> bool {
         pk.0.verify(msg, &self.0).is_ok()
     }
 }
