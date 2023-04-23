5c157291dd209334513bf841274bbfb92c6685f9
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-23 15:17:40 +0200

    cli: show the remote in a better table
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- radicle-cli/src/commands/remote/list.rs
+++ radicle-cli/src/commands/remote/list.rs
@@ -6,13 +6,20 @@ use crate::terminal as term;
 #[inline]
 fn format_direction(d: &git::Direction) -> String {
     match d {
-        git::Direction::Fetch => "fetch".to_owned(),
-        git::Direction::Push => "push".to_owned(),
+        git::Direction::Fetch => "↓".to_owned(),
+        git::Direction::Push => "↑".to_owned(),
     }
 }
 
 pub fn run(repo: &git::Repository) -> anyhow::Result<()> {
-    let mut table = Table::default();
+    let mut table = Table::new(term::table::TableOptions::bordered());
+    table.push([
+        term::format::dim(String::from("●")),
+        term::format::bold(String::from("Name")),
+        term::format::bold(String::from("Node ID")),
+    ]);
+    table.divider();
+
     let remotes = git::rad_remotes(repo)?;
     for r in remotes {
         for spec in r.refspecs() {
@@ -21,11 +28,11 @@ pub fn run(repo: &git::Repository) -> anyhow::Result<()> {
             let name = r.name.clone();
             let nid_row = url.namespace.map_or(
                 term::format::dim("This is the canonical upstream".to_string()),
-                |namespace| term::format::highlight(namespace.to_string()),
+                |namespace| term::format::dim(namespace.to_string()),
             );
             table.push([
-                term::format::badge_positive(format_direction(&dir)),
-                term::format::highlight(name.to_owned()),
+                term::format::tertiary(format_direction(&dir)),
+                term::format::bold(name.to_owned()),
                 nid_row,
             ]);
         }
