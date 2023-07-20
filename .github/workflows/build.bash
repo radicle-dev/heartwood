b4719b8c15980d63bc037678b85cb099d05538b6
Alexis Sellier <self@cloudhead.io>
2023-07-20 21:19:02 +0200

    ci: Fix man page build

--- .github/workflows/build.bash
+++ .github/workflows/build.bash
@@ -28,7 +28,7 @@ main () {
     cargo build --target="$target" --bin git-remote-rad --release
     cp target/"$target"/release/git-remote-rad "$staging"/
 
-    ./build-man-pages.sh "$staging" "$(find . -name '*.1.adoc')"
+    scripts/build-man-pages.sh "$staging" "$(find . -name '*.1.adoc')"
 
     tar czf "$staging.tar.gz" "$staging"
     cp "$staging.tar.gz" "$staging"/
