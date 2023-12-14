3cf51043b3c4f253334f175d7d14f8183ca7e05e
Fintan Halpenny <fintan.halpenny@gmail.com>
2023-12-14 09:37:05 +0000

    cli: list tracked and untracked remotes
    Add functionality for lising the remotes that are in the storage
    repository, but a working copy remote has not yet been created for
    them.
    
    This functionality is included in the `rad remote` command since it
    can be directly used for creating Git remotes using the same command,
    i.e. `rad remote add`.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>
    X-Clacks-Overhead: GNU Terry Pratchett

--- radicle-cli/src/commands/remote/list.rs
+++ radicle-cli/src/commands/remote/list.rs
@@ -1,29 +1,119 @@
+use std::collections::HashSet;
+
+use radicle::git::Url;
+use radicle::identity::{Did, Id};
+use radicle::node::{Alias, AliasStore as _, NodeId};
+use radicle::storage::ReadStorage as _;
+use radicle::Profile;
 use radicle_term::{Element, Table};
 
 use crate::git;
 use crate::terminal as term;
 
-pub fn run(repo: &git::Repository) -> anyhow::Result<()> {
-    let mut table = Table::default();
-    let remotes = git::rad_remotes(repo)?;
-    for r in remotes {
-        for (dir, url) in [("fetch", Some(r.url)), ("push", r.pushurl)] {
-            let Some(url) = url else {
-                continue;
-            };
+#[derive(Debug)]
+pub enum Direction {
+    Push(Url),
+    Fetch(Url),
+}
 
-            let description = url.namespace.map_or(
-                term::format::dim("(canonical upstream)".to_string()).italic(),
-                |namespace| term::format::tertiary(namespace.to_string()),
-            );
-            table.push([
-                term::format::bold(r.name.clone()),
-                description,
-                term::format::parens(term::format::secondary(dir.to_owned())),
-            ]);
+#[derive(Debug)]
+pub struct Tracked {
+    name: String,
+    direction: Option<Direction>,
+}
+
+impl Tracked {
+    fn namespace(&self) -> Option<NodeId> {
+        match self.direction.as_ref()? {
+            Direction::Push(url) => url.namespace,
+            Direction::Fetch(url) => url.namespace,
         }
     }
-    table.print();
+}
 
-    Ok(())
+#[derive(Debug)]
+pub struct Untracked {
+    remote: NodeId,
+    alias: Option<Alias>,
+}
+
+pub fn tracked(working: &git::Repository) -> anyhow::Result<Vec<Tracked>> {
+    Ok(git::rad_remotes(working)?
+        .into_iter()
+        .flat_map(|remote| {
+            [
+                Tracked {
+                    name: remote.name.clone(),
+                    direction: Some(Direction::Fetch(remote.url)),
+                },
+                Tracked {
+                    name: remote.name,
+                    direction: remote.pushurl.map(Direction::Push),
+                },
+            ]
+        })
+        .collect())
+}
+
+pub fn untracked<'a>(
+    rid: Id,
+    profile: &Profile,
+    tracked: impl Iterator<Item = &'a Tracked>,
+) -> anyhow::Result<Vec<Untracked>> {
+    let repo = profile.storage.repository(rid)?;
+    let aliases = profile.aliases();
+    let remotes = repo.remote_ids()?;
+    let git_remotes = tracked
+        .filter_map(|tracked| tracked.namespace())
+        .collect::<HashSet<_>>();
+    Ok(remotes
+        .filter_map(|remote| {
+            remote
+                .map(|remote| {
+                    (!git_remotes.contains(&remote)).then_some(Untracked {
+                        remote,
+                        alias: aliases.alias(&remote),
+                    })
+                })
+                .transpose()
+        })
+        .collect::<Result<Vec<_>, _>>()?)
+}
+
+pub fn print_tracked<'a>(tracked: impl Iterator<Item = &'a Tracked>) {
+    let mut table = Table::default();
+    for Tracked { direction, name } in tracked {
+        let Some(direction) = direction else {
+            continue;
+        };
+
+        let (dir, url) = match direction {
+            Direction::Push(url) => ("push", url),
+            Direction::Fetch(url) => ("fetch", url),
+        };
+        let description = url.namespace.map_or(
+            term::format::dim("(canonical upstream)".to_string()).italic(),
+            |namespace| term::format::tertiary(namespace.to_string()),
+        );
+        table.push([
+            term::format::bold(name.clone()),
+            description,
+            term::format::parens(term::format::secondary(dir.to_owned())),
+        ]);
+    }
+    table.print();
+}
+
+pub fn print_untracked<'a>(untracked: impl Iterator<Item = &'a Untracked>) {
+    let mut t = Table::default();
+    for Untracked { remote, alias } in untracked {
+        t.push([
+            match alias {
+                None => term::format::secondary("n/a".to_string()),
+                Some(alias) => term::format::secondary(alias.to_string()),
+            },
+            term::format::highlight(Did::from(remote).to_string()),
+        ])
+    }
+    t.print();
 }
