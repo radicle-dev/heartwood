449790e252869a9ea22ad139cc364424deeda1fb
cloudhead <cloudhead@radicle.xyz>
2024-02-23 17:57:32 +0100

    radicle: Fix indentation of SQL file

--- radicle/src/cob/cache/migrations/1.sql
+++ radicle/src/cob/cache/migrations/1.sql
@@ -1,19 +1,19 @@
 -- Issues
 create table if not exists "issues" (
-       -- Issue ID
-       "id"            text      primary key not null,
-       -- Repository ID
-       "repo"          text      not null,
-       -- Issue in JSON format
-       "issue"         text      not null
+  -- Issue ID
+  "id"            text      primary key not null,
+  -- Repository ID
+  "repo"          text      not null,
+  -- Issue in JSON format
+  "issue"         text      not null
 ) strict;
 
 -- Patches
 create table if not exists "patches" (
-       -- Patch ID
-       "id"            text      primary key not null,
-       -- Repository ID
-       "repo"          text      not null,
-       -- Patch in JSON format
-       "patch"         text      not null
+  -- Patch ID
+  "id"            text      primary key not null,
+  -- Repository ID
+  "repo"          text      not null,
+  -- Patch in JSON format
+  "patch"         text      not null
 ) strict;
