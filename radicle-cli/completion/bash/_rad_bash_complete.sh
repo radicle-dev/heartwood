4804af4a03c9f3d73790e4fdb498708e324d5006
Christopher Fredén <me@icetan.org>
2024-03-05 10:58:28 +0100

    cli: Add completion stubs for bash/zsh/fish

---
+++ radicle-cli/completion/bash/_rad_bash_complete.sh
@@ -0,0 +1,19 @@
+
+_clap_complete_rad() {
+    export IFS=$'\013'
+    export _CLAP_COMPLETE_INDEX=${COMP_CWORD}
+    export _CLAP_COMPLETE_COMP_TYPE=${COMP_TYPE}
+    if compopt +o nospace 2> /dev/null; then
+        export _CLAP_COMPLETE_SPACE=false
+    else
+        export _CLAP_COMPLETE_SPACE=true
+    fi
+    COMPREPLY=( $("rad" complete --shell bash -- "${COMP_WORDS[@]}") )
+    if [[ $? != 0 ]]; then
+        unset COMPREPLY
+    elif [[ $SUPPRESS_SPACE == 1 ]] && [[ "${COMPREPLY-}" =~ [=/:]$ ]]; then
+        compopt -o nospace
+    fi
+}
+complete -o nospace -o bashdefault -F _clap_complete_rad rad
+
