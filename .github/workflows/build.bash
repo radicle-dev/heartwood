fa4b929a06969578015ae2a8ed1124f64f7714a4
cloudhead <cloudhead@radicle.xyz>
2024-03-11 16:54:52 +0100

    ci: Remove old deploy script, cleanup build

--- .github/workflows/build.bash
+++ .github/workflows/build.bash
@@ -27,38 +27,38 @@ install() {
 }
 
 main () {
-    if [ $# -ne 1 ]; then
-        echo "$#: Wrong number of arguments"
-        exit 1
-    fi
+  if [ $# -ne 1 ]; then
+    echo "$#: Wrong number of arguments"
+    exit 1
+  fi
 
-    if ! command -v asciidoctor >/dev/null 2>&1; then
-      install
-    fi
+  if ! command -v asciidoctor >/dev/null 2>&1; then
+    install
+  fi
 
-    target="$1"
-    rustup target add "$target"
+  target="$1"
+  rustup target add "$target"
 
-    staging="radicle-$target"
-    mkdir -p "$staging"
+  staging="radicle-$target"
+  mkdir -p "$staging"
 
-    cargo build --target="$target" --package=radicle-httpd --release
-    cp target/"$target"/release/radicle-httpd "$staging"/
-    cp target/"$target"/release/rad-web "$staging"/
+  cargo build --target="$target" --package=radicle-httpd --release
+  cp target/"$target"/release/radicle-httpd "$staging"/
+  cp target/"$target"/release/rad-web "$staging"/
 
-    cargo build --target="$target" --package=radicle-node --release
-    cp target/"$target"/release/radicle-node "$staging"/
+  cargo build --target="$target" --package=radicle-node --release
+  cp target/"$target"/release/radicle-node "$staging"/
 
-    cargo build --target="$target" --bin rad --release
-    cp target/"$target"/release/rad "$staging"/
+  cargo build --target="$target" --bin rad --release
+  cp target/"$target"/release/rad "$staging"/
 
-    cargo build --target="$target" --bin git-remote-rad --release
-    cp target/"$target"/release/git-remote-rad "$staging"/
+  cargo build --target="$target" --bin git-remote-rad --release
+  cp target/"$target"/release/git-remote-rad "$staging"/
 
-    scripts/build-man-pages.sh "$staging" $(find . -name '*.1.adoc')
+  scripts/build-man-pages.sh "$staging" $(find . -name '*.1.adoc')
 
-    tar czf "$staging.tar.gz" "$staging"
-    cp "$staging.tar.gz" "$staging"/
+  tar czf "$staging.tar.gz" "$staging"
+  cp "$staging.tar.gz" "$staging"/
 }
 
 main "$@"
