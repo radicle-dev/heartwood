2331821a9169c031b885562c766c72635ad1055f
Fintan Halpenny <fintan.halpenny@gmail.com>
2025-08-08 13:13:04 +0100

    protocol: introduce fetcher service
    The `FetcherService` wraps the `FetcherState` alongside keeping track
    of subscribers for a given fetch.
    
    Typically, this is a channel for which the fetch result should be sent
    to.
    
    These subscribers are coalesced by maintaing a map from `(rid, node)`
    pairs to the subscriber.
    
    This continues the sans-IO approach where the type of subscribers are
    not specified, but rather are kept as a type parameter – to be
    specified by the caller.

--- crates/radicle-protocol/src/fetcher.rs
+++ crates/radicle-protocol/src/fetcher.rs
@@ -1,5 +1,12 @@
+pub mod service;
+pub use service::FetcherService;
+
 pub mod state;
 pub use state::{ActiveFetch, Config, FetcherState, MaxQueueSize, Queue, QueueIter, QueuedFetch};
 
 #[cfg(test)]
 mod test;
+
+// TODO(finto): `Service::fetch_refs_at` and the use of `refs_status_of` is a
+// layer above the `Fetcher` where it would perform I/O, mocked out by a trait,
+// to check if there are wants and add a fetch to the Fetcher.
