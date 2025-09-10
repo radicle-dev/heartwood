1ca9432871d51777187fce9968eac4d2a7a75351
Erik Kundt <erik@zirkular.io>
2025-09-10 23:51:34 +0200

    remote-helper: Remove `SyncWriter` in favor of `DrawTarget`

--- crates/radicle-remote-helper/src/push.rs
+++ crates/radicle-remote-helper/src/push.rs
@@ -898,16 +898,16 @@ fn sync(
     profile: &Profile,
 ) -> Result<(), cli::node::SyncError> {
     let progress = if io::stderr().is_terminal() {
-        cli::node::SyncWriter::Stderr(io::stderr())
+        term::DrawTarget::Stderr
     } else {
-        cli::node::SyncWriter::Sink
+        term::DrawTarget::Hidden
     };
     let result = cli::node::announce(
         repo,
         cli::node::SyncSettings::default().with_profile(profile),
         cli::node::SyncReporting {
             progress,
-            completion: cli::node::SyncWriter::Stderr(io::stderr()),
+            completion: term::DrawTarget::Stderr,
             debug: opts.sync_debug,
         },
         &mut node,
