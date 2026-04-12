474d6ccc13230b8d72d5d9e478e29648a0ea7511
Fintan Halpenny <fintan.halpenny@gmail.com>
2026-04-12 07:21:55 +0000

    radicle/git/repository: Define Revwalk trait
    Provides revision walk operations for walking over a commit graph.
    There are two methods:
    - `revwalk_oids`: returns an iterator of `Oid`s of the commit graph,
    - `revwalk_commits`: returns an iterator of `CommitData` of the commit graph.
    
    The `RevwalkPlan` describes a builder for translating into the
    adapters revision walk builder.

--- crates/radicle/src/git/repository.rs
+++ crates/radicle/src/git/repository.rs
@@ -7,13 +7,16 @@
 //! - [`types`] — Git domain types, i.e. Blob, Commit, TreeEntry, etc.
 //! - [`object`] — The Git object store; providing read and write capabilities of Git objects.
 //! - [`reference`] — The Git reference store; providing read and write capabilities of Git references.
+//! - [`revwalk`] – Git commit graph walk operations, i.e. "revwalk".
 //!
 //! [`reference`]: self::reference
 
 pub mod ancestry;
 pub mod object;
 pub mod reference;
+pub mod revwalk;
 pub mod types;
 
 pub use ancestry::{AheadBehind, Ancestry};
+pub use revwalk::{Revwalk, RevwalkPlan, SortOrder};
 pub use types::{Blob, Commit, ObjectKind, TreeEntry};
