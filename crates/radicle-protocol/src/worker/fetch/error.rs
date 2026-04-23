0fb337cddf8a0047ad8ef14e9ea2d6e08c734aca
Lorenz Leutgeb <lorenz.leutgeb@radicle.xyz>
2026-04-23 21:32:58 +0200

    protocol: Minimize scope of `radicle::git::raw`
    A tiny refactoring to make it less inviting to use
    `radicle::git::raw`.

--- crates/radicle-protocol/src/worker/fetch/error.rs
+++ crates/radicle-protocol/src/worker/fetch/error.rs
@@ -2,7 +2,7 @@ use std::io;
 
 use thiserror::Error;
 
-use radicle::{cob, git::raw, identity, storage};
+use radicle::{cob, identity, storage};
 use radicle_fetch as fetch;
 
 #[derive(Debug, Error)]
@@ -10,7 +10,7 @@ pub enum Fetch {
     #[error(transparent)]
     Run(#[from] fetch::Error),
     #[error(transparent)]
-    Git(#[from] raw::Error),
+    Git(#[from] radicle::git::raw::Error),
     #[error(transparent)]
     Storage(#[from] storage::Error),
     #[error(transparent)]
