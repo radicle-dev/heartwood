af27f7fbcc9cbb0224f3b25222662fcbc3a84590
Fintan Halpenny <fintan.halpenny@gmail.com>
2026-04-28 17:46:06 +0100

    git/repository/user: Add listing of DIDs
    Add `user::Namespaces` that provides a way to discover the DIDs that
    are associated with the repository through references.
    
    Since the current implementation uses `refs/namespaces` listing all
    the references provides multiple of the same `Did`.
    This can be managed by providing a filter for a well-known, unique
    referece, i.e. `rad/sigrefs`; thus, the API supports a filter.

--- crates/radicle/src/git/repository/user/error.rs
+++ crates/radicle/src/git/repository/user/error.rs
@@ -10,3 +10,15 @@ pub enum References {
     #[error(transparent)]
     ListRefs(#[from] reference::error::read::ListRefs),
 }
+
+/// Error returned by [`Namespaces::dids`] and [`Namespaces::dids_with_errors`].
+///
+/// [`Namespaces::dids`]: super::Namespaces::dids
+/// [`Namespaces::dids_with_errors`]: super::Namespaces::dids_with_errors
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum Dids {
+    /// Failed to initialise the reference iterator.
+    #[error(transparent)]
+    ListRefs(#[from] reference::error::read::ListRefs),
+}
